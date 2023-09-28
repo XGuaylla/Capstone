6bc7b50f3c5b0573678f5c467ad6dc04b4a0dbed
Xavier Guayllazaca <xavierguayllazaca@gmail.com>
2023-09-28 14:27:20 -0500

    Added image slider and started on box content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import { fetchProducts } from "./API"
+import ProductBox from "../components/ProductBox"
 
 export default function Home() {
     const [products, setProducts] = useState()
@@ -21,19 +22,42 @@ export default function Home() {
                     <span>Rating: {product.rating.rate} ({product.rating.count})</span>
                 </div>
             ))} */}
-            <div className="flex justify-between ml-2 mr-2 mt-8">
-                <div className="container w-1/5 h-40 bg-[#DDE6ED] rounded-lg flex justify-center items-center drop-shadow-xl text-3xl font-serif " >
-                    <h2>Best Selling</h2>
+            <div>
+                <div className="flex justify-between ml-2 mr-2 mt-8 ">
+                    <div className="box relative group border hover:bg-white rounded-lg w-1/5">
+                        <div className="boxContent  h-40 bg-[#DDE6ED] rounded-lg flex justify-center items-center drop-shadow-xl text-3xl font-serif">
+                            <h2>Best Selling</h2>
+                        </div>
+                        <div className="hoverContent w-98 mt-8 left-0 bg-blue-100 px-4 py-2 rounded-lg opacity-0 group-hover:opacity-100 transition duration-200 overflow-visible">
+                            <h2>Content</h2>
+                        </div>
+                    </div>
+                    <div className="box relative group border hover:bg-white rounded-lg w-1/5">
+                        <div className="boxContent  h-40 bg-[#DDE6ED] rounded-lg flex justify-center items-center drop-shadow-xl text-3xl font-serif">
+                            <h2>Top Rated</h2>
+                        </div>
+                        <div className="hoverContent w-98 mt-8 left-0 bg-blue-100 px-4 py-2 rounded-lg opacity-0 group-hover:opacity-100 transition duration-200 overflow-visible">
+                            <h2>Content</h2>
+                        </div>
+                    </div>
+                    <div className="box relative group border hover:bg-white rounded-lg w-1/5">
+                        <div className="boxContent  h-40 bg-[#DDE6ED] rounded-lg flex justify-center items-center drop-shadow-xl text-3xl font-serif">
+                            <h2>What's New</h2>
+                        </div>
+                        <div className="hoverContent w-98 mt-8 left-0 bg-blue-100 px-4 py-2 rounded-lg opacity-0 group-hover:opacity-100 transition duration-200 overflow-visible">
+                            <h2>Content</h2>
+                        </div>
+                    </div>
+                    <div className="box relative group border hover:bg-white rounded-lg w-1/5">
+                        <div className="boxContent  h-40 bg-[#DDE6ED] rounded-lg flex justify-center items-center drop-shadow-xl text-3xl font-serif">
+                            <h2>Coming Soon...</h2>
+                        </div>
+                        <div className="hoverContent w-98 mt-8 left-0 bg-blue-100 px-4 py-2 rounded-lg opacity-0 group-hover:opacity-100 transition duration-200 overflow-visible">
+                            <h2>Content</h2>
+                        </div>
+                    </div>
                 </div>
-                <div className="container w-1/5 h-40 bg-[#DDE6ED] rounded-lg flex justify-center items-center drop-shadow-xl text-3xl font-serif " >
-                    <h2>Top Rated</h2>
-                </div>
-                <div className="container w-1/5 h-40 bg-[#DDE6ED] rounded-lg flex justify-center items-center drop-shadow-xl text-3xl font-serif " >
-                    <h2>What's New</h2>
-                </div>
-                <div className="container w-1/5 h-40 bg-[#DDE6ED] rounded-lg flex justify-center items-center drop-shadow-xl text-3xl font-serif " >
-                    <h2>Coming Soon</h2>
-                </div>
+                
             </div>
         </div>
     )
